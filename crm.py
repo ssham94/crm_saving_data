7e82079916e8f23008e98376b05b87da73c1f1fb
Stanley Sham <stanley.sham94@gmail.com>
2019-07-23 14:42:10 -0400

    completed assignment

--- crm.py
+++ crm.py
@@ -47,7 +47,12 @@ class CRM:
         note = input()
 
         # call the appropriate method from the contact class (remember we imported it?):
-        Contact.create(first_name, last_name, email, note)
+        contact = Contact.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            note=note
+            )
 
     def modify_existing_contact(self):
         updated = False
@@ -59,22 +64,26 @@ class CRM:
             print('[4] Note')
             mod_choice = input('What would you like to change? ')
             if int(mod_choice) >= 1 and int(mod_choice) <= 4:
-                mod_contact = Contact.find(contact_id)
+                mod_contact = Contact.get(id= contact_id)
                 if int(mod_choice) == 1:
                     first_name_change = input('What would you like to change the first name to?: ')
-                    mod_contact.update('first_name', first_name_change)
+                    mod_contact.first_name = first_name_change
+                    mod_contact.save()
                     updated = True
                 elif int(mod_choice) == 2:
                     last_name_change = input('What would you like to change the last name to?: ')
-                    mod_contact.update('last_name', last_name_change)
+                    mod_contact.last_name = last_name_change
+                    mod_contact.save()
                     updated = True
                 elif int(mod_choice) == 3:
                     email_change = input('What would you like to change the email to?: ')
-                    mod_contact.update('email', email_change)
+                    mod_contact.email = email_change
+                    mod_contact.save()
                     updated = True
                 elif int(mod_choice) == 3:
                     note_change = input('What would you like to change the note to?: ')
-                    mod_contact.update('note', note_change)
+                    mod_contact.note = note_change
+                    mod_contact.save()
                     updated = True
             else:
                 print('Please enter one of the following numbers')
@@ -82,37 +91,36 @@ class CRM:
 
     def delete_contact(self):
         contact_id = int(input('Whose contact do you wish to edit? (type in ID please) '))
-        del_contact = Contact.find(contact_id)
-        del_contact.delete()
+        del_contact = Contact.get(id= contact_id)
+        del_contact.delete_instance()
         
     def display_all_contacts(self):
         print('')
-        Contact.all()
+        for contact in Contact.select():
+            print(contact.first_name)
         print('')
     
     def search_by_attribute(self):
         searched = False
-        attribute_dict = {1: 'first_name', 2:'last_name', 3: 'email', 4:'note'}
         while not searched:
-            print('[1] First Name')
-            print('[2] Last Name')
-            print('[3] Email')
-            print('[4] Note')
-            searched_attribute = int(input('What do you want to search by?: '))
-            if searched_attribute >= 1 and searched_attribute <= 4:
-                search_attribute_variable = input('Please enter the search term: ')
-                searched_contact = Contact.find_by(attribute_dict[searched_attribute], search_attribute_variable)
-                if searched_contact:
-                    print('')
-                    print(searched_contact)
-                    print('')
-                    searched = True
-                else:
-                    print('Please try again')
-            else:
-                print('Please enter one of the following numbers')
+            searched_attribute = (input('What do you want to search by?: '))
+            if searched_attribute == 'exit':
+                break
+            search_attribute_variable = input('Please enter the search term: ')
+            if searched_attribute == 'first_name':
+                searched_contact = Contact.get(first_name = search_attribute_variable)
+            elif searched_attribute == 'last_name':
+                searched_contact = Contact.get(last_name = search_attribute_variable)
+            elif searched_attribute == 'email':
+                searched_contact = Contact.get(email = search_attribute_variable)
+            if searched_contact:
+                print('')
+                print(searched_contact)
+                print('')
+                searched = True
+
+
 
                 
 a_crm_app = CRM()
 a_crm_app.main_menu()
-print(Contact.contacts.first_name)
